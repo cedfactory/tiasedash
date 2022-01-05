766417d975d0bd7b4670d492fc548e678dd06a07
cedfactory <78565328+cedfactory@users.noreply.github.com>
2022-01-05 11:58:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,50 +15,16 @@ fig = go.Figure(go.Candlestick(
         close=df['AAPL.Close']
     ))
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='Test Dataframe'
-tabtitle='test dataframe'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title=tabtitle
+app.title='test dataframe'
 
 ########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(myheading),
+    html.H1("Apple"),
     dcc.Graph(
         id='value',
         figure=fig
